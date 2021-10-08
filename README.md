0b285f8c945d5465fdaa76f452fa79c4ead27091
Darrell Tang <darrelltang@gmail.com>
2021-10-08 14:02:57 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flux k3s Homelab
+![High Level](diagrams/flux_k3s_lab.png)
--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -6,24 +6,20 @@ from diagrams.onprem.dns import Coredns
 from diagrams.onprem.monitoring import Prometheus,Grafana
 from diagrams.onprem.vcs import Gitlab
 
-with Diagram("Flux k3s Lab", show=True, direction="TB"):
+with Diagram("Flux k3s Lab", show=False, direction="TB"):
     with Cluster("k3s"):
-        fluxApps = ["helmController", "kustomizeController", "sourceController", "notificationController"]
-        networkingApps = ["metallb", "coredns", "pihole"]
-        monitoringApps = ["prometheus", "alertmanager", "grafana"]
-        homeAutomationApps = ["magicmirror"]
         with Cluster("flux-system"):
             flux = Flux("flux")
         with Cluster("networking"):
-            coredns = Coredns("Coredns")
-            pihole = Deployment("Pihole")
-            metalLB = Deployment("metalLB")
+            coredns = Coredns("Coredns\n10.0.0.237")
+            pihole = Deployment("Pihole\n10.0.0.235")
+            metalLB = Deployment("metalLB\n10.0.0230")
         with Cluster("monitoring"):
-            grafana = Grafana("Grafana")
-            prometheus = Prometheus("Prometheus")
+            grafana = Grafana("Grafana\n10.0.0.234")
+            prometheus = Prometheus("Prometheus\n10.0.0.233")
+            alertmanager = Deployment("Alertmanager\n10.0.0.232")
         with Cluster("home automation"):
-            magicmirror = Deployment("Magic Mirror")
-    gitlab = Gitlab("Gitlab")
+            magicmirror = Deployment("Magic Mirror\n10.0.0.236")
+    gitlab = Gitlab("Gitlab\nhttps://gitlab.com/darrelltang/flux-k3s/")
     gitlab >> flux
-    flux >> [metalLB, coredns, pihole, grafana, prometheus, magicmirror]
-    ingresses = ["Pihole", "Grafana", "Prometheus", "Magic Mirror"]
+    flux >> [metalLB, coredns, pihole, alertmanager, grafana, prometheus, magicmirror]
