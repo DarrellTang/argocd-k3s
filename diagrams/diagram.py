93abbd2dbf6b6567c524d22962cbb8a6b970ecbc
Darrell Tang <darrelltang@gmail.com>
2021-10-08 09:00:38 -0700

    diagrams

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -1,12 +1,10 @@
 from diagrams import Cluster, Diagram
-from diagrams.k8s.compute import Pod, StatefulSet, Deployment
-from diagrams.k8s.network import Service, Ingress
-from diagrams.k8s.storage import PV, PVC, StorageClass
+from diagrams.k8s.compute import Deployment
+from diagrams.k8s.network import Ingress
 from diagrams.onprem.gitops import Flux
 from diagrams.onprem.dns import Coredns
 from diagrams.onprem.monitoring import Prometheus,Grafana
 from diagrams.onprem.vcs import Gitlab
-from diagrams.generic.blank import Blank
 
 with Diagram("Flux k3s Lab", show=True, direction="TB"):
     with Cluster("k3s"):
@@ -20,21 +18,12 @@ with Diagram("Flux k3s Lab", show=True, direction="TB"):
             coredns = Coredns("Coredns")
             pihole = Deployment("Pihole")
             metalLB = Deployment("metalLB")
-            coredns >> pihole
         with Cluster("monitoring"):
             grafana = Grafana("Grafana")
             prometheus = Prometheus("Prometheus")
         with Cluster("home automation"):
             magicmirror = Deployment("Magic Mirror")
-        piholeIngress = Ingress("Pihole")
-        piholeIngress >> pihole
-        grafanaIngress = Ingress("Grafana")
-        grafanaIngress >> grafana
-        prometheusIngress = Ingress("Prometheus")
-        prometheusIngress >> prometheus
-        magicmirrorIngress = Ingress("Magic Mirror")
-        magicmirrorIngress >> magicmirror
     gitlab = Gitlab("Gitlab")
     gitlab >> flux
-    flux >> [coredns, pihole, grafana, prometheus, magicmirror]
+    flux >> [metalLB, coredns, pihole, grafana, prometheus, magicmirror]
     ingresses = ["Pihole", "Grafana", "Prometheus", "Magic Mirror"]
